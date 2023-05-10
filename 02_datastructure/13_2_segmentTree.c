ec0b84a7b16b322bce2fece556834b7cd4c08e37
dustjq1004 <dustjq1005@naver.com>
2023-05-10 15:50:54 +0900

    c 언어 기초 강의 1. 인덱스 트리 2. KMP 문자열 매칭 3. 라빈카프 문자열 매칭

---
+++ 02_datastructure/13_2_segmentTree.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NUMER 7
+
+int a[] = { 7, 1, 9, 5, 6, 4, 1};
+int tree[4 * NUMER];
+
+int init(int start, int end, int node) {
+    if (start == end) return tree[node] = a[start];
+    int mid = (start + end) /2;
+
+    return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+}
+
+int sum(int start, int end, int node, int left, int right) {
+    if(left > end || right < start) return 0;
+
+    if(left <= start && end <= right) return tree[node];
+
+    int mid = (start + end) / 2;
+    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
+void update(int start, int end, int node, int index, int dif) {
+    if(index < start || index > end) return ;
+
+    tree[node] += dif;
+    if(start == end)return ;
+    int mid = (start + end) / 2;
+    update(start, mid, node * 2, index, dif);
+    update(mid + 1, end, node * 2 + 1, index, dif);
+}
+
+int main(void) {
+    init(0, NUMER - 1, 1);
+    printf("0부터 6까지의 구간 합: %d\n", sum(0, NUMER - 1, 1, 0, 6));
+    printf("인덱스 5의 원소를 +3만큼 수정 \n");
+    update(0, NUMER - 1, 1, 5, 3);
+    printf("3부터 6까지의 구간 합: %d\n", sum(0, NUMER - 1, 1, 3, 6));
+    system("read");
+    return 0;
+}
